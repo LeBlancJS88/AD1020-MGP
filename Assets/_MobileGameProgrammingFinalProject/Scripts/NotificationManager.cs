3f6c22f05ec7875fdd3118c8f2d657775bd86cca
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2023-12-08 19:22:33 +0200

    Notifications Manager Update
    Finalized notifications messenger, changed cookie_icon to logo_icon and added resource_icon, pushing to test.

--- Assets/_MobileGameProgrammingFinalProject/Scripts/NotificationManager.cs
+++ Assets/_MobileGameProgrammingFinalProject/Scripts/NotificationManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NotificationManager : MonoBehaviour
 {
+    public UIManager uiManager;
+    public IdleManager idleManager;
+
     private readonly string channelId = "MyChannel";
 
     private void Awake()
@@ -22,20 +25,59 @@ public class NotificationManager : MonoBehaviour
     {
         if (paused)
         {
+            ScheduleBasicNotification();
+            ScheduleThreeHourNotification();
+            ScheduleSixHourNotification();
+        }
+    }
+
+    private void ScheduleBasicNotification()
+    {
+        AndroidNotification notification = new()
+        {
+            Title = "You'll be back...",
+            Text = "You'll be wasting your time anyway.",
+            FireTime = DateTime.Now.AddSeconds(5),
+            LargeIcon = "logo_large"
+        };
+        AndroidNotificationCenter.CancelScheduledNotification((int)NotificationID.Basic);
+        AndroidNotificationCenter.SendNotificationWithExplicitID(notification, channelId, (int)NotificationID.Basic);
+    }
+
+    private void ScheduleThreeHourNotification()
+    {
+        if (uiManager.stressGeneratorUnlocked || idleManager.anxietyResourceEnabled)
+        {
             AndroidNotification notification = new()
             {
-                Title = "You'll be back...",
-                Text = "You'll be wasting your time anyway.",
-                FireTime = DateTime.Now.AddSeconds(60),
-                LargeIcon = "cookie_large"
+                Title = "Generating Stress...",
+                Text = "You've built up 3 hours of stress.",
+                FireTime = DateTime.Now.AddHours(3),
+                LargeIcon = "resource_icon"
             };
-            AndroidNotificationCenter.CancelScheduledNotification((int)NotificationID.Basic);
-            AndroidNotificationCenter.SendNotificationWithExplicitID(notification, channelId, (int)NotificationID.Basic);
+            AndroidNotificationCenter.SendNotificationWithExplicitID(notification, channelId, (int)NotificationID.ThreeHour);
+        }
+    }
+
+    private void ScheduleSixHourNotification()
+    {
+        if (uiManager.stressGeneratorUnlocked || idleManager.anxietyResourceEnabled)
+        {
+            AndroidNotification notification = new()
+            {
+                Title = "Idle Mastery",
+                Text = "You've built up 6 hours of stress. Time to spend some.",
+                FireTime = DateTime.Now.AddHours(6),
+                LargeIcon = "resource_icon"
+            };
+            AndroidNotificationCenter.SendNotificationWithExplicitID(notification, channelId, (int)NotificationID.SixHour);
         }
     }
 
     public enum NotificationID
     {
-        Basic
+        Basic,
+        ThreeHour,
+        SixHour
     }
 }
